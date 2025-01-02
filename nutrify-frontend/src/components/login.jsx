bc4455b08425092e614ee40c6ecd26acd161ad1e
saravana3252 <sara18ec118@gmail.com>
2025-01-02 20:19:45 +0530

    ui updated

--- nutrify-frontend/src/components/login.jsx
+++ nutrify-frontend/src/components/login.jsx
@@ -27,23 +27,20 @@ function Login() {
     })
       .then((res) => {
         if (res.status === 404) {
-          setMessage({ type: 'error', text: 'email does not exist' });
+          setMessage({ type: 'error', text: 'Email does not exist' });
         } else if (res.status === 403) {
-          setMessage({ type: 'error', text: 'incorrect password' });
+          setMessage({ type: 'error', text: 'Incorrect password' });
         }
 
         setTimeout(() => {
-          setMessage({ type: 'invisible-msg', text: 'dummy text' });
+          setMessage({ type: 'invisible-msg', text: '' });
         }, 5000);
         return res.json();
       })
-
       .then((datas) => {
-        console.log(datas);
         if (datas.token !== undefined) {
           localStorage.setItem('nutrify-users', JSON.stringify(datas));
           loggedData.setloggedUser(datas);
-          console.log(loggedData);
           navigate('/home');
         }
       })
@@ -51,46 +48,47 @@ function Login() {
         console.log(err);
       });
   }
+
   function HandleInput(event) {
-    setData((prevData) => {
-      return { ...prevData, [event.target.name]: event.target.value };
-    });
+    setData((prevData) => ({
+      ...prevData,
+      [event.target.name]: event.target.value,
+    }));
   }
+
   return (
-    <>
-      <section className="form-container ">
-        <form className="form form-login" onSubmit={HandleSubmit}>
-          <h1>Start Your Fitness</h1>
-          <input
-            className="form-control"
-            type="email"
-            placeholder="enter your email"
-            required
-            onChange={HandleInput}
-            name="email"
-            value={data.email}
-          ></input>
-          <input
-            className="form-control"
-            type="password"
-            placeholder="enter your password"
-            required
-            maxLength={10}
-            onChange={HandleInput}
-            name="password"
-            value={data.password}
-          ></input>
-          <button className="btn btn-md btn-primary">LOGIN</button>
-          <p className="p-regis">
-            Not Registered ?
-            <Link className="link-regis" to="/register">
-              Register
-            </Link>
-          </p>
-          <p className={message.type}>{message.text}</p>
-        </form>
-      </section>
-    </>
+    <section className="form-container">
+      <form className="form form-login" onSubmit={HandleSubmit}>
+        <h1 className="form-heading">Start Your Fitness</h1>
+        <input
+          className="form-control"
+          type="email"
+          placeholder="Enter your email"
+          required
+          onChange={HandleInput}
+          name="email"
+          value={data.email}
+        />
+        <input
+          className="form-control"
+          type="password"
+          placeholder="Enter your password"
+          required
+          maxLength={10}
+          onChange={HandleInput}
+          name="password"
+          value={data.password}
+        />
+        <button className="btn btn-md btn-primary">LOGIN</button>
+        <p className="p-regis">
+          Not Registered?{' '}
+          <Link className="link-regis" to="/register">
+            Register
+          </Link>
+        </p>
+        <p className={message.type}>{message.text}</p>
+      </form>
+    </section>
   );
 }
 
