f2f5664e7b1223ae80f6b1e2bca3dab431376806
saravana3252 <sara18ec118@gmail.com>
2025-01-02 19:14:35 +0530

    backend updated

--- nutrify-backend/index.js
+++ nutrify-backend/index.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://127.0.0.1:5173/',
+  origin: 'http://127.0.0.1:5173',
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
