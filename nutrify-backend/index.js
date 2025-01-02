e0a9484db00f34aeb6410daf764f5d19f22b9e2a
saravana3252 <sara18ec118@gmail.com>
2025-01-02 19:49:51 +0530

    backend updated

--- nutrify-backend/index.js
+++ nutrify-backend/index.js
@@ -10,12 +10,7 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
 app.use(express.json());
-app.use(cors({
-  origin: 'http://127.0.0.1:5173',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
+app.use(cors())
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
